c9dd2b0d1aca476cbe4431de426e4df6f231d2ab
Fredrick Ojure Ogungo <Fredojure@gmail.com>
2013-05-07 15:19:18 +0200

    second commit

--- registerDealer.php
+++ registerDealer.php
@@ -5,19 +5,19 @@
         <title></title>
     </head>
     <body>
-        Create a new dealer account
+        Create a new dealer account<br/>
         <form action="newDealer" method="POST">
-         Email:
-         <input type="text" name="Email" value="" />
-         Confirm Email:
-         <input type="text" name="confirmEmail" value="" />
-         Choose a password:
-        <input type="text" name="password" value="" />
+         Email:<br/>
+         <input type="text" name="Email" value="" /><br/>
+         Confirm Email:<br/>
+         <input type="text" name="confirmEmail" value="" /><br/>
+         Choose a password:<br/>
+        <input type="text" name="password" value="" /><br/>
         Enter 8-20 letters and/or
-        number;case sensitive
+        number;case sensitive<br/>
         
-        Re-enter password:
-        <input type="text" name="confirmPassword" value="" />
+        Re-enter password:<br/>
+        <input type="text" name="confirmPassword" value="" /><br/>
         <input type="checkbox" name="termsAndConditions" value="ON" disabled="disabled" />
         I accept terms of the privacy statement and dealer agreement
         
